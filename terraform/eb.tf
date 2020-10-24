55e13b27a3574e13c1064182f047226f73a5f477
Jainish Pithadiya <jainish2008@gmail.com>
2020-10-24 18:21:28 +1100

    private signed s3 objects

--- terraform/eb.tf
+++ terraform/eb.tf
@@ -55,8 +55,7 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy_static_media" {
             "Effect": "Allow",
             "Action": "s3:GetObject",
             "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket_static.bucket}/static/*",
-                "arn:aws:s3:::${aws_s3_bucket.bucket_static.bucket}/media/*"
+                "arn:aws:s3:::${aws_s3_bucket.bucket_static.bucket}/static/*"
             ],
             "Principal":"*",
             "Condition": {
