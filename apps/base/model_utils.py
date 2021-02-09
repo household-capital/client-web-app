d5b2c795e1ad29ecfc684595f7ce9f120bbac045
Jainish Pithadiya <jainish2008@gmail.com>
2021-02-09 22:12:22 +1100

    HM-2097: Case addr search

--- apps/base/model_utils.py
+++ apps/base/model_utils.py
@@ -4,23 +4,16 @@ db_to_sf_map = {
     'base_specificity': 'unit',
     'street_number': 'streetNumber',
     'street_name': 'streetName',
-<<<<<<< HEAD
     'street_type': 'streetType',
     'gnaf_id': 'gnaf_id'
-=======
-    'street_type': 'streetType'
->>>>>>> HM-2097: Address field split
 }
 
 address_model_fields = list(db_to_sf_map.keys()) 
 
-<<<<<<< HEAD
 hidden_form_fields = [
     'gnaf_id'
 ]
 
-=======
->>>>>>> HM-2097: Address field split
 class AbstractAddressModel(models.Model): 
 
     class Meta:
@@ -29,9 +22,5 @@ class AbstractAddressModel(models.Model):
     base_specificity = models.CharField(max_length=20, blank=True, null=True) # Unit/Apartment/Street/lot
     street_number = models.CharField(max_length=10, blank=True, null=True) # reason why its not a int: Cater for 14A Street Name 
     street_name = models.CharField(max_length=80, blank=True, null=True)
-<<<<<<< HEAD
     street_type = models.CharField(max_length=80, blank=True, null=True)
     gnaf_id = models.CharField(max_length=80, blank=True, null=True)
-=======
-    street_type = models.CharField(max_length=80, blank=True, null=True)
->>>>>>> HM-2097: Address field split
