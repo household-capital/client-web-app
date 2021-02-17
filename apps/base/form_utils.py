0138a9834a63965ea03e1686a8af5c1f15e74e6e
Jainish Pithadiya <jainish2008@gmail.com>
2021-02-17 16:22:14 +1100

    wip

--- apps/base/form_utils.py
+++ apps/base/form_utils.py
@@ -1,7 +1,11 @@
-from apps.base.model_utils import address_model_fields
+from django import forms
+from apps.base.model_utils import address_model_fields, hidden_form_fields
 
 class AddressFormMixin():
     def __init__(self, *args, **kwargs):
         super(AddressFormMixin, self).__init__(*args, **kwargs)
         for field in address_model_fields:
             self.fields[field].disabled = True
+        for field in hidden_form_fields: 
+            self.fields[field].widget = forms.HiddenInput()
+            self.fields[field].label = False
--- apps/base/model_utils.py
+++ apps/base/model_utils.py
@@ -4,10 +4,15 @@ db_to_sf_map = {
     'base_specificity': 'unit',
     'street_number': 'streetNumber',
     'street_name': 'streetName',
-    'street_type': 'streetType'
+    'street_type': 'streetType',
+    'gnaf_id': 'gnaf_id'
 }
 
 address_model_fields = list(db_to_sf_map.keys()) 
+
+hidden_form_fields = [
+    'gnaf_id'
+]
 
 class AbstractAddressModel(models.Model): 
 
@@ -18,3 +23,4 @@ class AbstractAddressModel(models.Model):
     street_number = models.CharField(max_length=10, blank=True, null=True) # reason why its not a int: Cater for 14A Street Name 
     street_name = models.CharField(max_length=80, blank=True, null=True)
     street_type = models.CharField(max_length=80, blank=True, null=True)
+    gnaf_id = models.CharField(max_length=80, blank=True, null=True)
