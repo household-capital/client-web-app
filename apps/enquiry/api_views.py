66e7e70232b6d4c6775083e858543a14ed3e3124
Jainish Pithadiya <jainish2008@gmail.com>
2021-03-16 18:36:12 +1100

    lead_ingestion_patches: Notes

--- apps/enquiry/api_views.py
+++ apps/enquiry/api_views.py
@@ -48,12 +48,14 @@ class DataIngestion(APIView):
                 ).strftime('%d/%m/%Y')
             )
         if is_social: 
-            enquiryString += "\r\nMonth of Birth: {}".format(
-                json_payload.get('month_of_birth', '')
-            )
-            enquiryString += "\r\nYear of Birth: {}".format(
-                json_payload.get('age_status', '')
-            )
+            if json_payload.get('month_of_birth', ''):
+                enquiryString += "\r\nMonth of Birth: {}".format(
+                    json_payload.get('month_of_birth', '')
+                )
+            if json_payload.get('age_status', ''):
+                enquiryString += "\r\nYear of Birth: {}".format(
+                    json_payload.get('age_status', '')
+                )
             if json_payload.get('age_over_60'):
                 enquiryString += "\r\nOver 60?: {}".format(
                     json_payload.get('age_over_60')
