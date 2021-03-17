fad1b8f33ee0f0d54f58f894c8ae235a999eaf5f
Jainish Pithadiya <jainish2008@gmail.com>
2021-03-17 23:07:36 +1100

    HM-2255: salescahnnel

--- apps/case/model_utils.py
+++ apps/case/model_utils.py
@@ -55,6 +55,11 @@ def create_case_from_enquiry(enquiry, attach_to_case=True):
         'timestamp': 'enquiryCreateDate',
         'marketingSource': 'channelDetail'
     }
+    salesChannelMap = {
+        directTypesEnum.PARTNER.value: channelTypesEnum.PARTNER.value,
+        directTypesEnum.ADVISER.value: channelTypesEnum.ADVISER.value,
+        directTypesEnum.BROKER.value: channelTypesEnum.BROKER.value
+    }
     # Create dictionary of Case fields from Enquiry fields
     if ' ' in enquiry.name:
         firstname, surname = enquiry.name.split(' ', 1)
@@ -66,6 +71,10 @@ def create_case_from_enquiry(enquiry, attach_to_case=True):
     caseDict['surname_1'] = surname
     caseDict['caseStage'] = caseStagesEnum.UNQUALIFIED_CREATED.value
     caseDict['caseDescription'] = surname + " - " + str(enquiry.postcode)
+    if enquiry.referrer in salesChannelMap:
+        caseDict['salesChannel'] = salesChannelMap[enquiry.referrer]
+    else:
+        caseDict['salesChannel'] = channelTypesEnum.DIRECT_ACQUISITION.value
     for field in copyFields: 
         caseDict[field] = getattr(enquiry, field)
     for enq_field, case_field in map_fields.items():
