d17ee5ffcdf7728188f8bd85b35c5c69d9950bdd
Matt Chalmers <matt@barefootnomads.com>
2021-03-27 00:49:23 +1100

    HM-2440 - change Case to Lead

--- apps/case/views.py
+++ apps/case/views.py
@@ -641,7 +641,7 @@ class CaseAssignView(HouseholdLoginRequiredMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super(CaseAssignView, self).get_context_data(**kwargs)
-        context['title'] = 'Assign Case'
+        context['title'] = 'Assign Lead'
 
         return context
 
