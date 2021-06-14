c3aea0c5aa5a8f6ec2a006a8198fc3045bd5bf1a
Jainish Pithadiya <jainish2008@gmail.com>
2021-06-14 21:34:32 +1000

    HHC-464:wip

--- apps/case/views.py
+++ apps/case/views.py
@@ -283,6 +283,10 @@ class CaseDetailView(HouseholdLoginRequiredMixin, AddressLookUpFormMixin, Update
                 result = mappify.checkPostalAddress()
                 if result['status'] == 'Error':
                     messages.error(self.request, "Address validation. Please check address fields, or set address fields with find widget")
+        
+        if caseObj.lead_needs_action: 
+            messages.warning(self.request, "Lead needs to be actioned. Please review any new enquiries and mark lead as actioned")
+        
         return context
 
     def form_valid(self, form):
@@ -1374,13 +1378,14 @@ class MarkActionedView(HouseholdLoginRequiredMixin, View):
 
         caseUID = str(kwargs['uid'])
         caseObj = Case.objects.get(caseUID=caseUID)
-
-        if self.request.user.profile.isCreditRep == True:
-            caseObj.lead_needs_action = False
-            caseObj.save(update_fields=['lead_needs_action'])
-
+        if caseObj.owner is None: 
+            messages.error(self.request, "Lead must have owner before being actioned")
         else:
-            messages.error(self.request, "You must be a Credit Representative to mark as actioned")
+            if self.request.user.profile.isCreditRep == True:
+                caseObj.lead_needs_action = False
+                caseObj.save(update_fields=['lead_needs_action'])
+            else:
+                messages.error(self.request, "You must be a Credit Representative to mark as actioned")
 
         return HttpResponseRedirect(reverse_lazy('case:caseDetail', kwargs={'uid': caseObj.caseUID}))
 
