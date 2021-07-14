c889ffde805b104dafda26f8110fca48e14c9026
Jainish Pithadiya <jainish2008@gmail.com>
2021-07-14 10:09:05 +1000

    wip

--- apps/calendly/views.py
+++ apps/calendly/views.py
@@ -42,7 +42,7 @@ class CalendlyWebhook(View):
 
     def post(self, request, *args, **kwargs):
 
-        zoom_meeting_set = {'zoom'}
+        zoom_meeting_set = {'zoom', 'prequalification'}
         tracked_meeting_set = {'discovery', 'callback', 'phone'}
 
         #Get webhook ID from environmental settings
