2f5c62245bb2cf4fad23c021150be50e3f70d36f
Jainish <jainish2008@gmail.com>
2021-09-02 16:08:21 +1000

    nsa_turn_off

--- apps/case/model_utils.py
+++ apps/case/model_utils.py
@@ -131,8 +131,7 @@ def _build_case_data_update(enquiry, case=None):
         if field_val is not None and field_val != '':
             caseDict[case_field] = field_val
     if enquiry.marketingSource not in [
-        marketingTypesEnum.STARTS_AT_60.value,
-        marketingTypesEnum.NATIONAL_SENIORS.value
+        marketingTypesEnum.STARTS_AT_60.value
     ]:
         caseDict['lead_needs_action'] = True
     if is_create: 
