566d30ce5a7c5837548872e74613792ef2556389
PhillipTinsleyHHC <phillip.tinsley@householdcapital.com>
2022-05-31 10:35:38 +1000

    interest rate

--- apps/lib/site_Globals.py
+++ apps/lib/site_Globals.py
@@ -45,7 +45,7 @@ LOAN_LIMITS = {
 
 ECONOMIC = {'inflationRate': float(2.50),
             'housePriceInflation': float(3.00),
-            'interestRate': float(0.10),
+            'interestRate': float(0.35),
             'lendingMargin': float(4.85),
             'comparisonRateIncrement': float(0.03),
             'defaultMargin': float(2.00),
